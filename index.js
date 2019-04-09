ee2b92542311c5df8af01e836235d5aee9cf75ee
Mwangik <gkmwangi0@gmail.com>
2019-04-09 11:25:12 +0300

    added getand post genres routes and a joi validator function

---
+++ index.js
@@ -0,0 +1,41 @@
+const Joi = require('joi')
+const express = require('express');
+app = express();
+
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: 'Action' },  
+  { id: 2, name: 'Horror' },  
+  { id: 3, name: 'Romance' },  
+];
+
+// get list of genres
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
+
+// post a genre
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body); //object destructuring
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+// use joi to validate genre sent by client
+function validateGenre(genre) {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+  
+    return Joi.validate(genre, schema);
+  }
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
