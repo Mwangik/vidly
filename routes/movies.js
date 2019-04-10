6c119c3ca39a2b3c6efd2c04903b386a1cc5e5db
Mwangik <gkmwangi0@gmail.com>
2019-04-10 17:05:05 +0300

    added put,delete and get movie by id route handlers

--- routes/movies.js
+++ routes/movies.js
@@ -30,3 +30,47 @@ router.post('/', async (req, res) => {
   
   res.send(movie);
 });
+
+// update a movie
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+  
+    const movie = await Movie.findByIdAndUpdate(req.params.id,
+      { 
+        title: req.body.title,
+        genre: {
+          _id: genre._id,
+          name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+      }, { new: true });
+  
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    
+    res.send(movie);
+  });
+
+//   delete a movie
+router.delete('/:id', async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+  
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+  
+    res.send(movie);
+  });
+  
+//   get a particular movie by id
+router.get('/:id', async (req, res) => {
+    const movie = await Movie.findById(req.params.id);
+  
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+  
+    res.send(movie);
+  });
+  
+module.exports = router; 
