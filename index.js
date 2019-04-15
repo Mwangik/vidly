c1e795a3bd6ff2659df3d3f9893e08cbd1006c27
Mwangik <gkmwangi0@gmail.com>
2019-04-15 11:11:54 +0300

    added register user post route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose')
 const Fawn = require('fawn')
 const express = require('express');
+const users = require('.routes/users')
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies');
@@ -21,7 +22,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
-
+app.use('api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
