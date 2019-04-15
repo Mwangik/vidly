f45e8b1f558fbd96c68065b346c0c59092e26baf
Mwangik <gkmwangi0@gmail.com>
2019-04-15 12:19:46 +0300

    added authenticate users using jwt feature

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose')
@@ -8,9 +9,15 @@ const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const auth = require('./routes/auth');
 app = express();
 
 Fawn.init(mongoose);
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+  }
 
 // connecting to db
 mongoose.connect('mongodb://localhost/vidly')
@@ -23,6 +30,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
